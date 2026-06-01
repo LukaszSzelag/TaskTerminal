19763f1a8ad222f6723e4921b02829fba889e31a
Łukasz Szeląg <lukasz.szelag95@gmail.com>
2026-06-01 18:33:58 +0200

    Initial Commit

---
+++ TaskTerminal/TaskTerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTerminalApp.swift
+//  TaskTerminal
+//
+//  Created by Łukasz Szeląg on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct TaskTerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
